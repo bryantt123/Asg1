ce70602e46a9df5615107e95597e6d40e123df30
bryantt123 <s10242090@connect.np.edu.sg>
2022-12-04 05:31:08 +0800

    finished up the other pages

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("navbar").style.width="250px";
+    document.getElementById("openbtn").style.marginLeft="250px";
+}
+
+function closeNav(){
+    document.getElementById("navbar").style.width="0";
+    document.getElementById("openbtn").style.marginLeft="0";
+}
